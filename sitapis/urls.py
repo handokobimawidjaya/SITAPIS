df7874a4c1523a12e581499cb45234e43c67e979
Handoko Bimawidjaya <handokobima60@gmail.com>
2026-04-21 09:58:44 +0700

    feat: add 404 and 500 error pages, create manual book documentation, and update login UI and system configuration

--- sitapis/urls.py
+++ sitapis/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +14,7 @@ urlpatterns = [
     path('accounts/', include('apps.accounts.urls', namespace='accounts')),
     path('master/', include('apps.master.urls', namespace='master')),
     path('approval/', include('apps.approval.urls', namespace='approval')),
+    path('manual-book/', TemplateView.as_view(template_name='manual_book_sitapis.html'), name='manual_book'),
 ]
 
 if settings.DEBUG:
